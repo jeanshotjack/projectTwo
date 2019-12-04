6302a9ab4c5437831222d808ba03ead5adb83776
Sean Melvin <seanmelvin8595@gmail.com>
2019-12-03 19:16:18 -0500

    now only allows for unique usernames

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
     });
   });
   app.get("/posts/:id", function(req, res) {
-    db.Post.findOne({ where: { id: req.params.id } }).then(function(dbPost) {
+    db.Post.findAll({ where: { id: req.params.id } }).then(function(dbPost) {
       res.render("Post", {
         Post: dbPost
       });
@@ -19,45 +19,47 @@ module.exports = function(app) {
 
   app.post("/login", function(req, res) {
     var userInfo = req.body;
-
+    console.log("Posting");
     if (!userInfo.username || !userInfo.password) {
+      console.log("Fill in all fields");
       res.render("login", {
-        message: "Please Fill Out All Fields",
-        type: "error",
         username: userInfo.username
       });
     } else {
-      db.User.findOne({ username: userInfo.username }, function(err, user) {
-        if (err) {
-          res.render("login", {
-            message: "Database Error",
-            type: "error",
-            username: userInfo.username
-          });
-        }
-
-        if (user) {
-          var hash = crypto
-            .pbkdf2Sync(userInfo.password, user.salt, 10000, 64, "sha512")
-            .toString("hex");
-          if (hash === user.password) {
-            req.session.user = user;
-            res.redirect("/");
-          } else {
+      db.account
+        .findAll({ where: { username: userInfo.username } })
+        .then(function(username) {
+          console.log(username);
+          if (err) {
             res.render("login", {
-              message: "Invalid Password",
+              username: userInfo.username
+            });
+          }
+
+          if (user) {
+            var hash = crypto
+              .pbkdf2Sync(userInfo.password, user.salt, 10000, 64, "sha512")
+              .toString("hex");
+            if (hash === user.password) {
+              req.session.user = user;
+              console.log("Log in Successful");
+              res.redirect("/");
+            } else {
+              res.render("login", {
+                message: "Invalid Password",
+                type: "error",
+                username: userInfo.username
+              });
+            }
+          } else {
+            console.log("Log in Failed");
+            res.render("login", {
+              message: "User Does Not Exist",
               type: "error",
               username: userInfo.username
             });
           }
-        } else {
-          res.render("login", {
-            message: "User Does Not Exist",
-            type: "error",
-            username: userInfo.username
-          });
-        }
-      });
+        });
     }
   });
   app.post("/createaccount", function(req, res) {
@@ -76,11 +78,7 @@ module.exports = function(app) {
       });
     } else if (userInfo.password !== userInfo.confirm_password) {
       console.log("passwords don't match.");
-      res.render("signup", {
-        message: "Passwords do not match",
-        type: "error",
-        username: userInfo.username
-      });
+      res.render("signup");
     } else {
       console.log("Checking if user exists...");
       db.account
@@ -92,13 +90,10 @@ module.exports = function(app) {
         })
         .then(function(user) {
           console.log(user);
-
-          if (user.length < 0) {
+          console.log(user.length);
+          if (user.length === 1) {
             console.log("User already exists");
-            res.render("signup", {
-              message: "Username is Taken",
-              type: "error"
-            });
+            res.render("signup");
           } else {
             console.log("encrypting...");
             var salt = crypto.randomBytes(64).toString("hex");
@@ -117,10 +112,7 @@ module.exports = function(app) {
             db.account.create(newUser).then(function(user) {
               console.log("creating account");
               console.log("success");
-              res.render("login", {
-                message: "Account Created. Please Login.",
-                type: "success"
-              });
+              res.render("login");
             });
           }
         });
